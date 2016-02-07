ed294df38894a3694a151d0bf9796f99a84aa3b2
User <User@192.168.0.102>
2016-02-07 19:32:33 +0600

    Cikl

---
+++ Cikl1/src/wbrk1.java
@@ -0,0 +1,22 @@
+
+public class wbrk1 {
+	public static void main(String args[]){
+		//int[] m ={0, 1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40};
+		int[] m = new int[5];
+		for(int x=0;x<5;x++){
+			m[x]=x;
+			int[] m1 = new int[5];
+			for(int y=0;y<5;y++){
+				m1[y]=y;
+		System.out.print(m1[x]+"   ");
+		System.out.println(-y+"   ");
+		try{
+			Thread.sleep(500);
+			
+		}catch(Exception e){}
+		
+	}
+		System.out.println();
+	}
+	}
+}
